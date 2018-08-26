f498a357f75d9c2d67af7b91a1b8647290c7bbbd
m.moghadas <m.moghadas@hipotrip.com>
2018-08-26 18:42:36 +0430

    3rd ver.

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ app.use(function (req, res, next) {
 });
 
 app.get('/',function(req,res) {
+
+
   // find everything
   db.posters.find(function (err, docs) {
   	// docs is an array of all the documents in mycollection
@@ -33,25 +35,77 @@ app.get('/',function(req,res) {
 });
 
 
-app.post('/posters/add',function(req, res) {
-  // var newPoster ={
-  //   poster_id : md5(""+req.body['_package[package_seed]']),
-  //   poster_name : req.body['_package[package_seed]'],
-  //   validation_form_from : req.body._package.validation_form_from,
-  //   validation_form_to : req.body._package.validation_form_to,
-  //   packages : req.body._package.packages,
-  //   services : req.body._package.services
-  // };
 
-    console.log(req.body['_package[hotel]']);
-    // db.posters.insert(newPoster,function(err,res) {
-  //   if (err) {
-  //     console.log(err);
-  //   }
-  //   res.redirect('/');
-  // })
+function colloctHotels(params , num) {
+  dic = []
+
+  for (var i = 0; i < num; i++) {
+
+    var s1 = "_package[hotels]["+parseInt(i)+"][hotel_name]";
+    var s2 = "_package[hotels]["+parseInt(i)+"][number_of_star]";
+    var s3 = "_package[hotels]["+parseInt(i)+"][stay_place]";
+    var s4 = "_package[hotels]["+parseInt(i)+"][stay_duration]";
+    var s5 = "_package[hotels]["+parseInt(i)+"][one_bed_price]";
+    var s6 = "_package[hotels]["+parseInt(i)+"][two_bed_person]";
+
+    dic.push({
+      hotel_name: params[s1],
+      number_of_star: params[s2],
+      stay_place: params[s3],
+      stay_duration: params[s4],
+      one_bed_price: params[s5],
+      two_bed_person: params[s6]
+
+        });
+
+  }
+  return dic;
+}
+
+app.post('/posters/add',function(req, res) {
+
+
+  var newPoster ={
+    poster_id : md5(""+req.body['_package[package_seed]']),
+    phase : req.body['_package[phase]'],
+    date : req.body['_package[date]'],
+    hotels : colloctHotels(req.body, req.body['_package[number_of_hotel]'])
+  };
+
+  var num_res;
+
+  // db.posters.find(function (err, docs) {
+  //   num_res = docs.length
+  // });
+
+  // console.log( db.posters.count({"poster_id" : "123"},function(){}) );
+  // if (num_res === 0) {
+      db.packages.insert(newPoster, function () {
+      });
+
+      var upd;
+      db.packages.find(function (err, docs) {
+      	// docs is an array of all the documents in mycollection
+        res.json({ info: docs });
+        });
+
+        // return "upg";
+
+  // }
+  // else{
+    // db.posters.update({poster_id : newPoster['poster_id']}, {$set: {hotels:newPoster['hotels']}}, function (){} );
+  // }
+
+
 });
 
+
+// db.posters.insert(newPoster,function(err,res) {
+//   if (err) {
+//     console.log(err);
+//   }
+//   res.redirect('/');
+// })
 
 app.listen(3000,function() {
   console.log("on port 3000 Server is Running...");
