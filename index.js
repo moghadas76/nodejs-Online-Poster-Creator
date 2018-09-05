8aa3186b7d630a4bbcce4e00b6701d2facbaf199
moghadas76 <s.m.moghadas2012@gmail.com>
2018-09-05 18:25:32 +0430

    mvp version

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var mongojs = require('mongojs');
+const url = require('url');
 var path  = require('path');
 var bodyParser = require('body-parser');
 var md5 = require('md5');
-const fileUpload = require('express-fileupload');
+const multer = require('multer');
+const DIR_UPLOAD  = "uploads/";
 
 var db = mongojs('poster_app', ['posters','tmp']);
 var app = express()
 
-app.use(fileUpload());
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
@@ -23,13 +24,101 @@ app.use(function (req, res, next) {
   next();
 });
 
+
+// Set The Storage Engine
+const storage = multer.diskStorage({
+  destination: './public/uploads/',
+  filename: function(req, file, cb){
+    cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  }
+});
+
+// Init Upload
+const upload = multer({
+  storage: storage,
+  limits:{fileSize: 1000000},
+  fileFilter: function(req, file, cb){
+    checkFileType(file, cb);
+  }
+}).single('myImage');
+
+// Check File Type
+function checkFileType(file, cb){
+  // Allowed ext
+  const filetypes = /jpeg|jpg|png|gif/;
+  // Check ext
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  // Check mime
+  const mimetype = filetypes.test(file.mimetype);
+
+  if(mimetype && extname){
+    return cb(null,true);
+  } else {
+    cb('Error: Images Only!');
+  }
+}
+
+
+// Public Folder
+app.use(express.static('./public'));
+
+app.post('/debug',(req,res)=>{
+  console.log(req.body);
+});
+
+app.post('/color/:id',function (req,res) {
+  console.log(req.body);
+  db.poster.update({pos_id: req.params.id}, {$set: {color_id: req.body.color}}, {multi: true}, function () {
+    res.redirect("/"+req.params.id);
+  });
+});
+
+app.post('/update_name/:id',function (req,res) {
+  console.log(req.body);
+  db.poster.update({pos_id: req.params.id}, {$set: {name: req.body.pos_name}}, {multi: true}, function () {
+    res.redirect("/"+req.params.id);
+  });
+});
+
+app.post('/upload/:id',(req, res) => {
+  // console.log(req.file);
+  let id = req.params.id;
+  upload(req, res, (err) => {
+    if(err){
+      res.render('index', {
+        msg: err
+      });
+    } else {
+      if(req.file == undefined){
+        res.render('index', {
+          msg: 'Error: No File Selected!'
+        });
+      } else {
+        db.poster.update({pos_id: id}, {$set: {background_image: req.file.filename}}, {multi: true}, function () {
+        });
+        db.packages.find({poster_id:id},function (err, docs) {
+          var func = function (packages) {
+            db.items.find({poster_id:id},function (err, docs) {
+              console.log(docs,packages);
+              res.redirect('/'+id);
+            });
+          }
+          func(docs);
+        });
+      }
+    }
+  });
+});
+
+
 var i = 0;
 
 app.get('/home',function (req,res) {
   db.poster.find().toArray(function (err, docs) {
     // docs is an array of all the documents in mycollection
     res.render('home',{
-      poster: docs
+      poster: docs,
+      UPLOAD_DIR : "uploads/"
    });
  });
 });
@@ -38,7 +127,9 @@ app.get('/new',function(req,res) {
   var seed = md5(Math.floor((Math.random() * 100) + 1)+"");
   var new_pos = {
     pos_id:seed,
-    time : new Date()
+    time : new Date(),
+    background_image : null,
+    name: null
   };
   db.poster.insert(new_pos,function(err,response) {
     res.redirect('/'+seed);
@@ -50,18 +141,36 @@ app.get('/:id',function(req,res) {
 
   var doc=[],serv=[];
   db.packages.find({poster_id:req.params.id},function (err, docs) {
-    var func = function (packages) {
+    // var func = function (packages) {
+      var packages = docs;
+      console.log("1");
       db.items.find({poster_id:req.params.id},function (err, docs) {
-        console.log(docs,packages);
-        res.render('index',{
-          poster_id: req.params.id,
-          title: 'Posters',
-         posters: packages,
-         items : docs
-       });
+        // var func_image = function (packages,items) {
+          var items = docs;
+          console.log("2");
+          db.poster.findOne({pos_id:req.params.id},function (err,pos) {
+            console.log("3");
+            console.log(packages,items,pos);
+            var f = function (pos) {
+              if(pos){
+                res.render('index',{
+                  poster_id: req.params.id,
+                  title: 'Posters',
+                  file: DIR_UPLOAD + pos.background_image,
+                  back_color : pos.color_id,
+                  posters: packages,
+                  items : items
+                });
+            }
+            }
+            f(pos);
+
+          });
+        // };
+        // func_image(packages,docs);
       });
-    }
-    func(docs);
+    // }
+    // func(docs);
   });
 });
 
@@ -122,23 +231,7 @@ app.post('/items/add/:id',function (req,res) {
   });
 })
 
-app.post('/upload', function(req, res) {
-  if (!req.files)
-    return res.status(400).send('No files were uploaded.');
 
-  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-  let sampleFile = req.files.sampleFile;
-
-  console.log(sampleFile.mv);
-  // Use the mv() method to place the file somewhere on your server
-  sampleFile.mv('/images', function(err) {
-    if (err){
-      console.log(err);
-      return res.status(500).send(err);
-    }
-    res.send('File uploaded!');
-  });
-});
 
 app.listen(3000,function() {
   console.log("on port 3000 Server is Running...");
